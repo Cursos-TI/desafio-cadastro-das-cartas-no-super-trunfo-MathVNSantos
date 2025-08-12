ca9544e07181f459188d8ec03d1a2169148ca883
Matheus Santos <desenv.mathsantos@gmail.com>
2025-08-12 15:45:23 -0300

    Alterar variáveis estado e código

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -54,7 +54,7 @@
 int main() {
 // Declaração de variáveis
     char estado1 = 'A', estado2 = 'B';
-    char codigo1[2] = '01', codigo2[2] = '02';
+    char codigo1[3] = "01", codigo2[3] = "02";
     char nome_da_cidade1[50], nome_da_cidade2[50];
     int populacao1, populacao2;
     float area1, area2;
@@ -66,8 +66,8 @@ int main() {
     printf("Carta 1 \n");
     //Atribui-se valores definidos para as variaveis estado e código.
     //Os Estados e os Códigos já são pré-definidos. Aparecerão apenas para identificação.
-    printf("Estado: %c\n", estado1);
-    printf("Código: %c%c\n", estado1, codigo1);
+    printf("Estado: %c \n", estado1); //Saída: Estado: B
+    printf("Código: %c%s\n", estado1, codigo1); //Saída: Código: B02
     
     printf("Nome da Cidade: ");
     scanf("%s", nome_da_cidade1);
@@ -79,7 +79,7 @@ int main() {
     scanf("%f", &area1);
 
     printf("PIB: ");
-    scanf("%f", &float PIB1);
+    scanf("%f", &PIB1);
 
     printf("Pontos Turísticos: ");
     scanf ("%d", &pontos_turísticos1);
@@ -90,8 +90,8 @@ int main() {
     printf("Carta 2 \n");
     //Atribui-se valores definidos para as variaveis estado e código.
     //Os Estados e os Códigos já são pré-definidos. Aparecerão apenas para identificação.
-    printf("Estado: %c\n", estado1);
-    printf("Código: %c%c\n", estado1, codigo1);
+    printf("Estado: %c\n", estado2); // Saída: Estado: B
+    printf("Código: %c%s\n", estado2, codigo2); // Saída: Código: B02
     
     printf("Nome da Cidade: ");
     scanf("%s", nome_da_cidade2);
