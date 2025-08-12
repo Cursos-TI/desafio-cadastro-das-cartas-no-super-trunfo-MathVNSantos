bcfccaa26b332a50088ddb25c1618ecb3ea19839
Matheus Santos <desenv.mathsantos@gmail.com>
2025-08-12 16:21:46 -0300

    Mudança entrada e saída de dados

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -63,23 +63,23 @@ int main() {
 
 
 //Solicitação de dados da primeira carta.
-    printf("Carta 1 \n");
+    printf("Carta 1\n");
     //Atribui-se valores definidos para as variaveis estado e código.
     //Os Estados e os Códigos já são pré-definidos. Aparecerão apenas para identificação.
-    printf("Estado: %c \n", estado1); //Saída: Estado: B
+    printf("Estado: %c\n", estado1); //Saída: Estado: B
     printf("Código: %c%s\n", estado1, codigo1); //Saída: Código: B02
     
     printf("Nome da Cidade: ");
-    scanf("%s", nome_da_cidade1);
+    scanf("%s", &nome_da_cidade1);
 
     printf("População: ");
     scanf("%d", &populacao1);
 
     printf("Área (km²): ");
-    scanf("%f", &area1);
+    scanf("%.2f", &area1);
 
     printf("PIB: ");
-    scanf("%f", &PIB1);
+    scanf("%.2f", &PIB1);
 
     printf("Pontos Turísticos: ");
     scanf ("%d", &pontos_turísticos1);
@@ -87,7 +87,7 @@ int main() {
     printf("---------------------------\n");
 
     //Solicitação de dados da segunda carta.
-    printf("Carta 2 \n");
+    printf("Carta 2\n");
     //Atribui-se valores definidos para as variaveis estado e código.
     //Os Estados e os Códigos já são pré-definidos. Aparecerão apenas para identificação.
     printf("Estado: %c\n", estado2); // Saída: Estado: B
