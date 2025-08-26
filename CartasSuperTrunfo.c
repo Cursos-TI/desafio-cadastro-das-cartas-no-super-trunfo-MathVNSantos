810d60da9ef86b670d56d9109827be142af48622
Matheus Santos <desenv.mathsantos@gmail.com>
2025-08-26 13:01:58 -0300

    remover comentários iniciais

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,55 +1,5 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
-
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
-
-    /* O que você vai fazer
-    Criar um programa em C que permita ao usuário inserir os dados de duas cartas do Super Trunfo. Para cada carta, o usuário deverá fornecer as seguintes informações:
-
-    - Estado: Uma letra de 'A' a 'H' (representando um dos oito estados). Tipo: char
-    - Código da Carta: A letra do estado seguida de um número de 01 a 04 (ex: A01, B03). Tipo: char[] (um array de caracteres, ou string)
-    - Nome da Cidade: O nome da cidade. Tipo: char[] (string)
-    - População: O número de habitantes da cidade. Tipo: int
-    - Área (em km²): A área da cidade em quilômetros quadrados. Tipo: float
-    - PIB: O Produto Interno Bruto da cidade. Tipo: float
-    - Número de Pontos Turísticos: A quantidade de pontos turísticos na cidade. Tipo: int
-    
-    Após o usuário inserir os dados de cada carta, seu programa deve exibir na tela as informações cadastradas, de forma organizada e legível. 
-    Para cada carta, imprima cada informação em uma linha separada, com uma descrição clara. Por exemplo:
-
-    Carta 1:
-    Estado: A
-    Código: A01
-    Nome da Cidade: São Paulo
-    População: 12325000
-    Área: 1521.11 km²
-    PIB: 699.28 bilhões de reais
-    Número de Pontos Turísticos: 50
-
-    Carta 2:
-    Estado: B
-    Código: B02
-    Nome da Cidade: Rio de Janeiro
-    População: 6748000
-    Área: 1200.25 km²
-    PIB: 300.50 bilhões de reais
-    Número de Pontos Turísticos: 30 
-
-    */
 
 int main() {
 // Declaração de variáveis
