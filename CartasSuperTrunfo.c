2d877e84360eb9e83f8211d9436528d6b2415b4c
Matheus Santos <desenv.mathsantos@gmail.com>
2025-09-02 15:21:01 -0300

    código completo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -121,11 +121,11 @@ printf("-----Carta 2-----\n\n");
         (populacaoA > populacaoB) * 1 + (populacaoB > populacaoA) * 2,
         (populacaoA > populacaoB));
 
-    printf("Área: Carta %d venceu (%.2f)\n",
-        (AreaA > AreaB) * 1 + (AreaB > AreaA) * 2,)
+    printf("Área: Carta %d venceu (%d)\n",
+        (AreaA > AreaB) * 1 + (AreaB > AreaA) * 2,
         (AreaA > AreaB));                           
 
-    printf("PIB: Carta %d venceu (%.2f)\n"
+    printf("PIB: Carta %d venceu (%.d)\n",
         (PIBA > PIBB) * 1 + (PIBB > PIBA) * 2,
         (PIBA > PIBB));
     
@@ -133,19 +133,19 @@ printf("-----Carta 2-----\n\n");
         (pontos_turisticosA > pontos_turisticosB) * 1 + (pontos_turisticosB > pontos_turisticosA) * 2,
         (pontos_turisticosA > pontos_turisticosB));
     
-    printf("Densidade Demográfica: Carta %d venceu (%.2f)\n",
+    printf("Densidade Demográfica: Carta %d venceu (%d)\n",
         (densidadeA < densidadeB) * 1 + (densidadeB < densidadeA) * 2,
         (densidadeA < densidadeB));
 
-    printf("PIB per capita: Carta %d venceu (%.2f)\n",
+    printf("PIB per capita: Carta %d venceu (%d)\n",
         (pib_per_capitaA > pib_per_capitaB) * 1 + (pib_per_capitaB > pib_per_capitaA) * 2,
         (pib_per_capitaA > pib_per_capitaB));
 
-    printf("Super Poder: Carta %d venceu (%.2f)\n",
+    printf("Super Poder: Carta %d venceu (%d)\n",
         (super_poderA > super_poderB) * 1 + (super_poderB > super_poderA) * 2,
         (super_poderA > super_poderB));
 
-    
-        return 0;
+    //FimAlgoritimo
+    return 0;
 
 }
